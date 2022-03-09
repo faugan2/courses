d703167604583102d2952c42ad1786e59ddad44a
faugan <foganbidi@gmail.com>
2022-03-09 21:50:55 +0000

    changes

---
+++ src/features/counterSlice.js
@@ -0,0 +1,29 @@
+import {  createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+ 
+    
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  
+  
+});
+
+export const { incrementByAmount } = counterSlice.actions;
+
+
+export const selectCount = (state) => state.counter.value;
+
+
+
+export default counterSlice.reducer;
